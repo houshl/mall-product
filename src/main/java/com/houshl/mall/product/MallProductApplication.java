763661d96eaa2f1f0d7f96fb56f79e516b6f1218
侯双龙 <slhou@lc.com>
2018-07-23 10:46:38 +0800

    commit

---
+++ src/main/java/com/houshl/mall/product/MallProductApplication.java
@@ -0,0 +1,14 @@
+package com.houshl.mall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallProductApplication.class, args);
+	}
+}
